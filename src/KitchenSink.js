6233b970b67b38b1430f6c987df65eb878340777
Simon Vaucher <s.vaucher@applicaster.com>
2020-03-09 17:08:55 +0200

    Sheesh

--- src/KitchenSink.js
+++ src/KitchenSink.js
@@ -13,6 +13,7 @@ const _styles = {
     padding: 8
   },
   text: {
+    fontSize: 15,
     color: "rgba(0,0,0,1.0)"
   }
 };
@@ -20,30 +21,38 @@ const _styles = {
 export function KitchenSink({ component, styles, cellStyles }) {
   const { configuration } = cellStyles[component.cell_plugin_configuration_id];
 
-  const textStyle = {
-    fontFamily: R.path(
-      ["styles", "my_style_key", "android_style", "my_font"],
-      configuration
-    ),
-    color: R.path(
-      ["styles", "my_style_key", "android_style", "my_color"],
-      configuration
-    ),
-    fontSize: parseInt(
-      R.path(
-        ["styles", "my_style_key", "android_style", "my_font_size"],
-        configuration
-      ),
-      10
-    ),
-    padding: 18
-  };
-
   return function DrawCell({ item: { title }, state }) {
     return (
       <View style={_styles.container}>
-        <Text style={textStyle}>Item Title</Text>
+        <Text style={_styles.text}>{JSON.stringify(configuration)}</Text>
       </View>
     );
   };
+
+  // const textStyle = {
+  //   fontFamily: R.path(
+  //     ["styles", "my_style_key", "android_style", "my_font"],
+  //     configuration
+  //   ),
+  //   color: R.path(
+  //     ["styles", "my_style_key", "android_style", "my_color"],
+  //     configuration
+  //   ),
+  //   fontSize: parseInt(
+  //     R.path(
+  //       ["styles", "my_style_key", "android_style", "my_font_size"],
+  //       configuration
+  //     ),
+  //     10
+  //   ),
+  //   padding: 18
+  // };
+
+  // return function DrawCell({ item: { title }, state }) {
+  //   return (
+  //     <View style={_styles.container}>
+  //       <Text style={textStyle}>Item Title</Text>
+  //     </View>
+  //   );
+  // };
 }
