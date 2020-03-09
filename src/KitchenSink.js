0ef501672b948868d025096ce297d82f0524f73d
Simon Vaucher <s.vaucher@applicaster.com>
2020-03-09 14:16:50 +0200

    restart from scratch

--- src/KitchenSink.js
+++ src/KitchenSink.js
@@ -1,6 +1,5 @@
-// @flow
+import React from "react";
 import { Text, View } from "react-native";
-import { extractConfiguration } from "./utils";
 
 const _styles = {
   container: {
@@ -15,24 +14,11 @@ const _styles = {
   }
 };
 
-/**
- *
- * Creates master from the provided configuration
- * @param {Object} props
- * @param {Object} props.component
- * @param {Object} props.styles
- * @param {Object} props.cellStyles
- * @returns {Function} Functional React Component to render the masterCell with the provided options
- */
-// $FlowFixMe
 export function KitchenSink({ component, styles, cellStyles }) {
-  const configurationStyles = extractConfiguration({ component, cellStyles });
-
   return function DrawCell({ item: { title }, state }) {
     return (
       <View styles={_styles.container}>
-        <Text styles={_styles.text}>{title}</Text>
-        <Text styles={_styles.text}>configuration:</Text>
+        <Text styles={_styles.text}>hello there</Text>
       </View>
     );
   };
